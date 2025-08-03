05f39b171e3a8710df8f4cc377ae925e92a44a17
Devendra2005-n <dnimbalkar340@gmail.com>
2025-08-03 19:32:30 +0530

    Update vite.config.js

--- project/vite.config.js
+++ project/vite.config.js
@@ -1,6 +1,12 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // Important for Vercel
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  }
 })
